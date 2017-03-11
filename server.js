1785239d18f08263199080b54bac8b142bf06a5d
sarathkella <sarathchandra231@gmail.com>
2017-03-11 22:07:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'artical1.html'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
